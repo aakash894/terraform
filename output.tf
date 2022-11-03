a0913102f224125153c316b4cd28ab5fd94993cf
aakash894 <113110858+aakash894@users.noreply.github.com>
2022-11-03 22:42:29 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -38,6 +38,10 @@ output "private_route_id" {
   value = aws_route_table.private.id
 }
 
+output "Intance_public_ip" {
+  value = aws_instance.db_client.public_ip
+}
+
 output "Instance_ips" {
   value = "${formatlist("%v", aws_instance.db_node.*.private_ip)}"
 }
