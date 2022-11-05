07170248edebf47fcf8e705a6f45ca6c503c2166
aakash894 <113110858+aakash894@users.noreply.github.com>
2022-11-05 10:48:53 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -2,17 +2,20 @@
 set -x
 USER=$1
 Key_path=$2
-IP=$(terraform output -json Instance_ips | jq -r '.[]')
-echo $IP
-touch Invnetory ~/tool-infra
+touch datafile
+echo "" > datafile
+IP=$(terraform output -json Instance_ips | jq -r '.[]') 
+echo $IP > datafile
+touch Invnetory 
 echo "" > Invnetory
 echo "[seed-nodes]" > Invnetory
-for line in $IP
+for line in $(cat datafile)
 do
     y=$(echo "${line}") 
     IP=$(echo $y | awk -F, '{print $1}')
-    echo "node${j} ansible_host=${IP} ansible_user=${USER} ansible_ssh_private_key_file=${Key_path} ansible_ssh_common_args='-o StrictHostKeyChecking=no'" >> Invnetory 
-    ((j++))
+    echo "node${j} ansible_host=${IP} ansible_user=${USER} ansible_ssh_private_key_file=${Key_path}" >> Invnetory
+    ((j++)) 
     ((i++))
 done
 cat Invnetory
+rm datafile
